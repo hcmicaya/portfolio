805aead7c7b6aef4ba38a64f46ff0f2cd65a217b
Hans Micaya <hcmicaya@gmail.com>
2022-03-12 19:12:06 -0500

    update project list

--- src/data.js
+++ src/data.js
@@ -1,50 +1,50 @@
 export const products = [
-	{
-		id: 1,
-		title: 'HansCamp',
-		desc: 'Mongo JavaScript Express Bootstrap',
-		img: 'https://i.imgur.com/AgqZXxQ.png',
-		link: 'https://hanscamp.herokuapp.com/',
-		source: 'https://github.com/hcmicaya/YelpCamp2.0'
-	},
-	{
-		id: 2,
-		title: 'HansStore',
-		desc: 'React Redux',
-		img: 'https://i.imgur.com/y4zVnDb.png',
-		link: 'http://hansstore.herokuapp.com/',
-		source: 'https://github.com/hcmicaya/crwn-clothing'
-	},
-	{
-		id: 3,
-		title: 'E-Comm',
-		desc: 'Mongo Express JavaScript',
-		img: 'https://i.imgur.com/jKdhbGD.png',
-		link: 'https://hans-ecomm.herokuapp.com/',
-		source: 'https://github.com/hcmicaya/ecomm'
-	},
-	{
-		id: 4,
-		title: 'Movie x Movie',
-		desc: 'JavaScript',
-		img: 'https://i.imgur.com/WZrMkh6.png',
-		link: 'https://moviexmovie.herokuapp.com/',
-		source: 'https://github.com/hcmicaya/MovieFight'
-	},
-	{
-		id: 5,
-		title: 'Grid-Maze',
-		desc: 'JavaScript',
-		img: 'https://i.imgur.com/HEFzPlt.png',
-		link: 'https://grid-maze.herokuapp.com/',
-		source: 'https://github.com/hcmicaya/Maze'
-	},
-	{
-		id: 6,
-		title: 'Alien Invasion',
-		desc: 'Pygame',
-		img: 'https://i.imgur.com/IeQkTqL.jpg',
-		link: 'https://github.com/hcmicaya/alien_invasion',
-		source: 'https://github.com/hcmicaya/alien_invasion'
-	}
+    {
+        id: 1,
+        title: "HansCamp",
+        desc: "Mongo JavaScript Express Bootstrap",
+        img: "https://i.imgur.com/AgqZXxQ.png",
+        link: "https://hanscamp.herokuapp.com/",
+        source: "https://github.com/hcmicaya/YelpCamp2.0",
+    },
+    {
+        id: 2,
+        title: "HansStore",
+        desc: "React Redux",
+        img: "https://i.imgur.com/y4zVnDb.png",
+        link: "http://hansstore.herokuapp.com/",
+        source: "https://github.com/hcmicaya/crwn-clothing",
+    },
+    {
+        id: 3,
+        title: "DevCamper API",
+        desc: "Node.js Express MongoDB",
+        img: "https://i.imgur.com/84HGF4A.jpg",
+        link: "https://hcmicaya.github.io/devcamper/",
+        source: "https://github.com/hcmicaya/devcamper",
+    },
+    {
+        id: 4,
+        title: "E-Comm",
+        desc: "Mongo Express JavaScript",
+        img: "https://i.imgur.com/jKdhbGD.png",
+        link: "https://hans-ecomm.herokuapp.com/",
+        source: "https://github.com/hcmicaya/ecomm",
+    },
+    {
+        id: 5,
+        title: "Movie x Movie",
+        desc: "JavaScript",
+        img: "https://i.imgur.com/WZrMkh6.png",
+        link: "https://moviexmovie.herokuapp.com/",
+        source: "https://github.com/hcmicaya/MovieFight",
+    },
+    {
+        id: 6,
+        title: "Grid-Maze",
+        desc: "JavaScript",
+        img: "https://i.imgur.com/HEFzPlt.png",
+        link: "https://grid-maze.herokuapp.com/",
+        source: "https://github.com/hcmicaya/Maze",
+    },
 ];
